d60ffbb9752e6e2ef7a4342dd73a3e7fd76c8858
jason-xuan <zjxuan1996@outlook.com>
2017-06-19 15:20:41 +0800

    API 添加成功

---
+++ AviationManagement/Controllers/AreasController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AviationManagement;
+using AviationManagement.Models;
+
+namespace AviationManagement.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Areas")]
+    public class AreasController : Controller
+    {
+        private readonly WebAPIDbContext _context;
+
+        public AreasController(WebAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Areas
+        [HttpGet]
+        public IEnumerable<Area> GetAreas()
+        {
+            return _context.Areas;
+        }
+
+        // GET: api/Areas/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetArea([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var area = await _context.Areas.SingleOrDefaultAsync(m => m.AreaID == id);
+
+            if (area == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(area);
+        }
+
+        // PUT: api/Areas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArea([FromRoute] string id, [FromBody] Area area)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != area.AreaID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(area).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AreaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Areas
+        [HttpPost]
+        public async Task<IActionResult> PostArea([FromBody] Area area)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Areas.Add(area);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArea", new { id = area.AreaID }, area);
+        }
+
+        // DELETE: api/Areas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArea([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var area = await _context.Areas.SingleOrDefaultAsync(m => m.AreaID == id);
+            if (area == null)
+            {
+                return NotFound();
+            }
+
+            _context.Areas.Remove(area);
+            await _context.SaveChangesAsync();
+
+            return Ok(area);
+        }
+
+        private bool AreaExists(string id)
+        {
+            return _context.Areas.Any(e => e.AreaID == id);
+        }
+    }
+}
